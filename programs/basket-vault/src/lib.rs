ad314e332e25a74f5ad32a49d7e0f93744a80137
MallorcaBCDays <44105033+MallorcaBCDays@users.noreply.github.com>
2026-09-12 00:34:17 +0200

    Update lib.rs

--- programs/basket-vault/src/lib.rs
+++ programs/basket-vault/src/lib.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("11111111111111111111111111111111");
 
+const MAX_CONSTITUENTS: usize = 10;
+
 #[program]
 pub mod basket_vault {
     use super::*;
@@ -9,11 +11,33 @@ pub mod basket_vault {
     pub fn initialize_basket(
         ctx: Context<InitializeBasket>,
         name: String,
+        constituents: Vec<Constituent>,
     ) -> Result<()> {
+        require!(
+            !constituents.is_empty(),
+            BasketError::NoConstituents
+        );
+
+        require!(
+            constituents.len() <= MAX_CONSTITUENTS,
+            BasketError::TooManyConstituents
+        );
+
+        let total_weight: u16 = constituents
+            .iter()
+            .map(|c| c.weight_bps)
+            .sum();
+
+        require!(
+            total_weight == 10_000,
+            BasketError::InvalidWeights
+        );
+
         let basket = &mut ctx.accounts.basket;
 
         basket.creator = ctx.accounts.creator.key();
         basket.name = name;
+        basket.constituents = constituents;
         basket.bump = ctx.bumps.basket;
 
         Ok(())
@@ -45,5 +69,31 @@ pub struct Basket {
     #[max_len(64)]
     pub name: String,
 
+    #[max_len(10)]
+    pub constituents: Vec<Constituent>,
+
     pub bump: u8,
+}
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    Clone,
+    InitSpace
+)]
+pub struct Constituent {
+    pub mint: Pubkey,
+    pub weight_bps: u16,
+}
+
+#[error_code]
+pub enum BasketError {
+    #[msg("Basket must contain at least one constituent.")]
+    NoConstituents,
+
+    #[msg("Basket cannot contain more than 10 constituents.")]
+    TooManyConstituents,
+
+    #[msg("Constituent weights must add up to 10,000 basis points.")]
+    InvalidWeights,
 }
