5b834601f7245c399b33334d99467355b9a1f1a0
MallorcaBCDays <44105033+MallorcaBCDays@users.noreply.github.com>
2026-09-12 00:32:16 +0200

    Update lib.rs

--- programs/basket-vault/src/lib.rs
+++ programs/basket-vault/src/lib.rs
@@ -6,11 +6,44 @@ declare_id!("11111111111111111111111111111111");
 pub mod basket_vault {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("Basket Vault initialized");
+    pub fn initialize_basket(
+        ctx: Context<InitializeBasket>,
+        name: String,
+    ) -> Result<()> {
+        let basket = &mut ctx.accounts.basket;
+
+        basket.creator = ctx.accounts.creator.key();
+        basket.name = name;
+        basket.bump = ctx.bumps.basket;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeBasket<'info> {
+    #[account(
+        init,
+        payer = creator,
+        space = 8 + Basket::INIT_SPACE,
+        seeds = [b"basket", creator.key().as_ref()],
+        bump
+    )]
+    pub basket: Account<'info, Basket>,
+
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Basket {
+    pub creator: Pubkey,
+
+    #[max_len(64)]
+    pub name: String,
+
+    pub bump: u8,
+}
